6c6d4d3e14bc15a2ee5c26b758829161acf60443
ThomasMurat <thomasmurat1992@gmail.com>
2020-07-07 09:25:59 +0200

    up des fichiers

---
+++ LAMANU-cookie-law.php
@@ -0,0 +1,7 @@
+/*
+Plugin Name: LAMANU-cookie-law
+Version: 0.1
+Plugin URI: http://github.com/AmauriC/tarteaucitron.js/
+Description: WordPress Plugin for european cookie law.
+Author: ThomasMurat URI: https://github.com/ThomasMurat
+*/
